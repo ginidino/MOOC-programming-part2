c156991ce2055d9d6aa72bd6347984f2f3240c57
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-04-19 17:07:59 +0900

    Add files via upload

---
+++ WEEK7/Exercise04/Container.java
@@ -0,0 +1,50 @@
+package Week7;
+
+import java.util.ArrayList;
+
+//Exercise 4: Thing, Suitcase, and Container
+public class Container {
+	private ArrayList<Suitcase> suitcases;
+	private int maxWeight;
+
+	// Exercise 4.6: Container
+	public Container(int maxWeight) {
+		// which is given the maximum weight limit
+		this.suitcases = new ArrayList<Suitcase>();
+		this.maxWeight = maxWeight;
+	}
+	// Exercise 4.6: Container
+	public void addSuitcase(Suitcase suitcase) {
+		// adds the suitcase as a parameter to the container
+		int weight = 0;
+		for (Suitcase sc : suitcases) {
+			weight += sc.totalWeight();
+		}
+        if (this.maxWeight - weight >= suitcase.totalWeight()) {
+            this.suitcases.add(suitcase);
+        }
+	}
+	
+	public int totalWeight() {
+		int weight = 0;
+        for (Suitcase suitcase : suitcases) {
+            weight += suitcase.totalWeight();
+        }
+		return weight;
+	}
+	// Exercise 4.7: The Container Contents
+	public void printThings() {
+		for (Suitcase suitcase : suitcases) {
+			suitcase.printThings();
+		}
+	}
+	// Exercise 4.6: Container
+	public String toString() {
+		// returns a string in the form "x suitcases (y kg)"
+		int weight = 0;
+		for (Suitcase suitcase : suitcases) {
+			weight += suitcase.totalWeight();
+		}
+		return suitcases.size() + " suitcases " + "(" + weight + " kg)";
+	}
+}

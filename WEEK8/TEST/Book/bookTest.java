c6ca44df45465e6d2a4770a86478962fe34b729b
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-02 17:16:35 +0900

    Update bookTest.java

--- WEEK8/TEST/Book/bookTest.java
+++ WEEK8/TEST/Book/bookTest.java
@@ -69,12 +69,12 @@ public class bookTest {
 		// HashCode Method
 		HashMap<Book, String> loaners = new HashMap<Book, String>();
 
-        Book objectbook = new Book("Objectbook", 2000);
-        loaners.put(objectbook, "Pekka");
-        loaners.put(new Book("Test Driven Development",1999), "Arto");
+        	Book objectbook = new Book("Objectbook", 2000);
+        	loaners.put(objectbook, "Pekka");
+        	loaners.put(new Book("Test Driven Development",1999), "Arto");
 
-        System.out.println(loaners.get(objectbook));
-        System.out.println(loaners.get(new Book("Objectbook", 2000)));
-        System.out.println(loaners.get(new Book("Test Driven Development", 1999)));
+        	System.out.println(loaners.get(objectbook));
+        	System.out.println(loaners.get(new Book("Objectbook", 2000)));
+        	System.out.println(loaners.get(new Book("Test Driven Development", 1999)));
 	}
 }

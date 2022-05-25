a3ee9c55eff22799e560c5d3942af1ca3ea4898c
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-25 12:07:57 +0900

    Add files via upload

---
+++ WEEK9/Exercise17/mooc.ui/TextUserInterface.java
@@ -0,0 +1,9 @@
+package mooc.ui;
+
+// Exercise 17: First Packages
+// Exercise 17.2: Text User Interface
+public class TextUserInterface implements UserInterface {
+	public void update() {
+		System.out.println("Updating the user interface");
+	}
+}

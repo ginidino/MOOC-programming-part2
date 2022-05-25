a73fb33d4cb9bf724941ae588c3d67b2867179c7
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-25 12:09:56 +0900

    Create ApplicationLogic.java

---
+++ WEEK9/Exercise17/mooc.logic/ApplicationLogic.java
@@ -0,0 +1,20 @@
+package mooc.logic;
+
+import mooc.ui.UserInterface;
+
+// Exercise 17: First Packages
+// Exercise 17.3: Application Logic
+public class ApplicationLogic {
+	private UserInterface ui;
+	
+	public ApplicationLogic(UserInterface ui) {
+		this.ui = ui;
+	}
+	
+	public void execute(int howManyTimes) {
+		for (int i = 0; i < howManyTimes; i++) {
+			System.out.println("The application logic works");
+			ui.update();
+		}
+	}
+}

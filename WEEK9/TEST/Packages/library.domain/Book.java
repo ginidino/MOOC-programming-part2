6e5d970ded1113b7a9280d2fa095589b9edadc7d
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-25 11:58:23 +0900

    Update and rename WEEK9/TEST/Packages/Packages to WEEK9/TEST/Packages/library.domain/Book.java

---
+++ WEEK9/TEST/Packages/library.domain/Book.java
@@ -0,0 +1,13 @@
+package library.domain;
+
+public class Book {
+	private String name;
+	
+	public Book(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}

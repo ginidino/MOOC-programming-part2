63df3ce23d3b8b5a8d7c2190ba72995055ced0c5
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-05-28 16:25:48 +0900

    Add files via upload

---
+++ WEEK9/Exercise19/Exercise19.java
@@ -0,0 +1,13 @@
+package Week09;
+
+// Exercise 19: Method Argument Validation
+public class Exercise19 {
+	public static void main(String[] args) {
+		// Exercise 19.1: Person Validation
+		Person person = new Person("Paul", 26);
+		System.out.println(person.getName() + " is " + person.getAge());
+		
+		// Exercise 19.2: Calculator Validation
+		Calculator cal = new Calculator();
+	}
+}

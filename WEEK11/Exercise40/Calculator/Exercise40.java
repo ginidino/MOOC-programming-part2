10a4aca03d85c31cb485089be9a6e231ed3230b0
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-07-28 17:27:11 +0900

    Create Exercise40.java

---
+++ WEEK11/Exercise40/Calculator/Exercise40.java
@@ -0,0 +1,11 @@
+package Week11;
+
+import javax.swing.SwingUtilities;
+
+// Exercise 40: Calculator
+public class Exercise40 {
+	public static void main(String[] args) {
+		GraphicCalculator calc = new GraphicCalculator();
+		SwingUtilities.invokeLater(calc);
+	}
+}

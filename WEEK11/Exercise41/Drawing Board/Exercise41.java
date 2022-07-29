66e5de944a3da994f7021423efb23a814515f90f
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-07-29 17:57:39 +0900

    Create Exercise41.java

---
+++ WEEK11/Exercise41/Drawing Board/Exercise41.java
@@ -0,0 +1,11 @@
+package Week11;
+
+import javax.swing.SwingUtilities;
+
+// Exercise 41: Drawing Board
+public class Exercise41 {
+	public static void main(String[] args) {
+		Ex41UserInterface ui = new Ex41UserInterface();
+        SwingUtilities.invokeLater(ui);
+	}
+}
